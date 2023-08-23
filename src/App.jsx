da1c8c867c63a9c2b57ff9025453f97298e8a9aa
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-23 09:44:44 +0300

    posts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import PostsPage from './pages/posts/PostsPage';
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/posts' element={<PostsPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/posts/PostsPage.jsx
@@ -0,0 +1,11 @@
+export default function PostsPage() {
+  return (
+    <div className='container'>
+      <h1>PostsPage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        corporis, reiciendis eius ipsa laborum sed!
+      </p>
+    </div>
+  );
+}
