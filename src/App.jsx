93843209d12caf5ecb64dfd8dc462d007a4a8f38
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-23 09:57:30 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import PostsPage from './pages/posts/PostsPage';
+import Header from './components/layout/Header';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/posts' element={<PostsPage />} />
