9a9bdf5754c26bdff62f3ac1fd7c837b4c3e0adb
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-23 09:42:10 +0300

    HomePage + Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 export default function App() {
   return (
     <div>
-      <h1>New topic</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div className='container'>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        corporis, reiciendis eius ipsa laborum sed!
+      </p>
+    </div>
+  );
+}
