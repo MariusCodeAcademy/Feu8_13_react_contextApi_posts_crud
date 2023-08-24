955e11828cca9b6c1d985cf353f9eefa4cb45ddc
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-24 09:00:03 +0300

    home page container

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
+import Container from '../components/UI/container/Container';
+
 export default function HomePage() {
   return (
-    <div className='container'>
+    <Container>
       <h1>HomePage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
         corporis, reiciendis eius ipsa laborum sed!
       </p>
-    </div>
+    </Container>
   );
 }
